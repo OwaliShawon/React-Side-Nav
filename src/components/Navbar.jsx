08f65b58c2d7340d17c32a0c1e371ecd0a2251d2
owalishawon <owali.shawon@gmail.com>
2022-01-05 18:43:34 -0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// icons from react icons package
+import { } from "react-icon/md";
+
+
+const Navbar = () => {
+    return (
+        <div className="styles.navbar_container">
+            NAVBAR
+        </div>
+    );
+};
+
+export default Navbar;
